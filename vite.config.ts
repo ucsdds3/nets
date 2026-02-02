7eb2487a4a38cfe38660a87734e99972cbf99713
Roberto Echeverria <145717389+Robert-ech@users.noreply.github.com>
2026-02-01 21:19:31 -0800

    changed config to set base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    
     // ...other plugins like react()
   ],
+  base: "/nets/",
 });
